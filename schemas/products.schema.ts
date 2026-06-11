4fddb48a7614bae2dfdfa62c58defd75a646f933
Alvaro Acha <alvaro.acha@unosquare.com>
2026-06-10 20:59:47 -0400

    test: add tests to get and post products

---
+++ schemas/products.schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const ProductsSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  price: z.number(),
+  description: z.string(),
+  category: z.string(),
+  image: z.string(),
+});
